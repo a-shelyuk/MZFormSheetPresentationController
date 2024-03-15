99ea0219bdb46486f4f562523b1d50b14fc4f4ee
Artem <artem.shelyuk@ourbond.com>
2024-03-15 17:53:17 -0400

    Add SPM support

---
+++ Sources/MZFormSheetPresentationController/include/MZFormSheetPresentationControllerFramework.h
@@ -0,0 +1,34 @@
+//
+//  MZFormSheetPresentationControllerFramework.h
+//  MZFormSheetPresentationControllerFramework
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MZFormSheetPresentation.
+FOUNDATION_EXPORT double MZFormSheetPresentationControllerVersionNumber;
+
+//! Project version string for MZFormSheetPresentation.
+FOUNDATION_EXPORT const unsigned char MZFormSheetPresentationControllerVersionString[];
+
+// In this header, you should import all the public headers of 
+// your framework using statements like 
+// #import "PublicHeader.h"
+
+#import "MZFormSheetPresentationController.h"
+#import "MZFormSheetPresentationViewController.h"
+#import "MZFormSheetPresentationViewControllerSegue.h"
+
+#import "MZTransition.h"
+
+#import "MZFormSheetPresentationViewControllerAnimator.h"
+#import "MZFormSheetPresentationViewControllerAnimatedTransitioning.h"
+#import "MZFormSheetPresentationViewControllerInteractiveAnimator.h"
+#import "MZFormSheetPresentationViewControllerInteractiveTransitioning.h"
+
+#import "MZFormSheetPresentationContentSizing.h"
+#import "MZFormSheetContentSizingNavigationController.h"
+#import "MZFormSheetContentSizingNavigationControllerAnimator.h"
+
+#import "MZBlurEffectAdapter.h"
+#import "UIViewController+TargetViewController.h"
